e41bd4e9a1cd5a421d69476e338097c181ed371f
Iñigo Alonso Fernandez <inigoalonso@inigoalonso.com>
2024-07-04 09:55:40 +0200

    2.15

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -62,8 +62,15 @@ const App = () => {
   const addPerson = (event) => {
     event.preventDefault();
     const newPerson = { name: newName, number: newNumber };
-    if (persons.some(person => person.name === newName)) {
-      alert(`${newName} is already added to phonebook`);
+    const existingPerson = persons.find(person => person.name === newName);
+    if (existingPerson) {
+      if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
+        personService.update(existingPerson.id, newPerson).then(returnedPerson => {
+          setPersons(persons.map(person => (person.id !== existingPerson.id ? person : returnedPerson)));
+          setNewName('');
+          setNewNumber('');
+        });
+      }
     } else {
       personService
         .create(newPerson)
