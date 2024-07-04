1f96930152b336be5e182fbc7fde4b8cc8a69657
Iñigo Alonso Fernandez <inigoalonso@inigoalonso.com>
2024-07-04 02:34:46 +0200

    1.13

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -13,15 +13,24 @@ const App = () => {
   ]
    
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0));
 
   const getRandomAnecdote = () => {
     const randomIndex = Math.floor(Math.random() * anecdotes.length);
     setSelected(randomIndex);
   };
 
+  const voteAnecdote = () => {
+    const votesCopy = [...votes];
+    votesCopy[selected] += 1;
+    setVotes(votesCopy);
+  };
+
   return (
     <div>
     <p>{anecdotes[selected]}</p>
+    <p>has {votes[selected]} votes.</p>
+    <button onClick={voteAnecdote}>vote</button>
     <button onClick={getRandomAnecdote}>next anecdote</button>
     </div>
   )
