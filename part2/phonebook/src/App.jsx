0faeb42c3246bb9da69712861b177d27cfd93240
Iñigo Alonso Fernandez <inigoalonso@inigoalonso.com>
2024-07-04 09:26:54 +0200

    2.12

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -66,7 +66,13 @@ const App = () => {
     if (persons.some(person => person.name === newName)) {
       alert(`${newName} is already added to phonebook`);
     } else {
-      setPersons(persons.concat(newPerson));
+      axios
+        .post('http://localhost:3001/persons', newPerson)
+        .then(response => {
+          setPersons(persons.concat(response.data));
+          setNewName('');
+          setNewNumber('');
+        });
     }
     setNewName('');
     setNewNumber('');
